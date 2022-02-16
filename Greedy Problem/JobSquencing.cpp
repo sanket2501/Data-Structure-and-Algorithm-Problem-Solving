f2f2f84a8e7e734b5eba5209e2fb6b5a642f78a7
sanket2501 <69619339+sanket2501@users.noreply.github.com>
2022-02-16 23:16:16 +0530

    Update JobSquencing.cpp

--- Greedy Problem/JobSquencing.cpp
+++ Greedy Problem/JobSquencing.cpp
@@ -1,4 +1,5 @@
 /*
+Difficulty - Medium
 Given a set of N jobs where each jobi has a deadline and profit associated with it.
 Each job takes 1 unit of time to complete and only one job can be scheduled at a time. We earn the profit associated with job if and only if the job is completed by its deadline.
 Find the number of jobs done and the maximum profit.
