2fdd95427c7cbc439648948f05d17a2da1187a0c
sanket2501 <sanketshrawagi86999@gmail.com>
2022-06-13 23:22:16 +0530

    Easy Stack

---
+++ Array/ImplementTwoStacksInAnArra.cpp
@@ -0,0 +1,28 @@
+/*
+Difficulty Easy
+
+Your task is to implement  2 stacks in one array efficiently.
+
+Example 1:
+
+Input:
+push1(2)
+push1(3)
+push2(4)
+pop1()
+pop2()
+pop2()
+
+Output:
+3 4 -1
+
+Explanation:
+push1(2) the stack1 will be {2}
+push1(3) the stack1 will be {2,3}
+push2(4) the stack2 will be {4}
+pop1()   the poped element will be 3 
+from stack1 and stack1 will be {2}
+pop2()   the poped element will be 4 
+from stack2 and now stack2 is empty
+pop2()   the stack2 is now empty hence -1 .
+*/
