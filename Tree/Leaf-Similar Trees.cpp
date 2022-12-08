ade88763809564bb95c41d81d80c76cd59f7add6
sanket2501 <sanketshrawagi86999@gmail.com>
2022-12-08 18:48:30 +0530

    Easy

---
+++ Tree/Leaf-Similar Trees.cpp
@@ -0,0 +1,44 @@
+/*
+Easy
+Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+
+For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+
+Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+
+Example 1:
+
+Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+Output: true
+Example 2:
+
+Input: root1 = [1,2,3], root2 = [1,3,2]
+Output: false
+
+*/
+
+/*
+
+class Solution {
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> leaves1;
+        vector<int> leaves2;
+        dfs(root1, leaves1);
+        dfs(root2, leaves2);
+
+        return leaves1 == leaves2;
+    }
+
+    void dfs(TreeNode* node, vector<int>& leaves) {
+        if (node == NULL) return;
+        if (node->left == NULL && node->right == NULL)
+            leaves.push_back(node->val);
+        dfs(node->left, leaves);
+        dfs(node->right, leaves);
+    }
+};
+
+*/
